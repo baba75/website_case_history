f394f1c404b14a1a07db744b6a10f555c08c706a
Alberto <baba75@gmail.com>
2020-02-28 17:36:04 +0100

    css layout fixing

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     'website': "https://www.ecobeton.it",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Website',
     'version': '0.2',
